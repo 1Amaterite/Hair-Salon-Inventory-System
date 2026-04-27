c50b23ba6690396dee13b1c30571fe219535890e
Pete <pete_navato@dlsu.edu.ph>
2026-04-27 08:21:18 +0800

    Restart

--- backend/controllers/userController.js
+++
@@ -1,9 +0,0 @@
-const getUsers = (req, res) => {
-    res.send('Get all users');
-};
-
-const createUser = (req, res) => {
-    res.send('Create a user');
-};
-
-module.exports = { getUsers, createUser };
